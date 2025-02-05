d93c34c1edefc942e4fdf288cd70b15b6dedab90
Mark Hafemann <markhafemann1@gmail.com>
2025-02-05 15:25:42 -0600

    update dependencies and improve VSCode settings for better development experience

--- .svelte-kit/generated/client/nodes/1.js
+++ .svelte-kit/generated/client/nodes/1.js
@@ -1,1 +1,1 @@
-export { default as component } from "../../../../node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
+export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.5.0_@sveltejs+vite-plugin-svelte@3.0.0_svelte@4.2.7_vite@5.0.3__svelte@4.2.7_vite@5.0.3/node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
